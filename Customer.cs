97b5cdf3fc1b41fe666463f86c7c4c81203846c1
jpeterseni35 <jpeterseni35@gmail.com>
2021-10-25 18:46:33 -0500

    LearningMethods

--- Customer.cs
+++ Customer.cs
@@ -9,22 +9,29 @@ namespace CSharpMosh
         public string Name;
         public List<Order> Orders;
 
+        // Constructors
         public Customer()
         {
+
             Orders = new List<Order>();
+            // Constructor using object inintialization
+
         }
         public Customer(int id)
-            : this()
+            : this()  // only use this if you have to
         {
             this.Id = id;
         }
 
         public Customer(int id, string name)
-            : this(id)
+            : this(id) // only use this if you have to
 
         {
 
             this.Name = name;
         }
+
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,9 +6,42 @@ namespace CSharpMosh
     {
         static void Main(string[] args)
         {
-            var customer = new Customer(1, "Jake");
+
+            var customer = new Customer
+            {
+                Id = 2,
+                Name = "Jim"
+            };
             System.Console.WriteLine(customer.Id);
             System.Console.WriteLine(customer.Name);
+
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            System.Console.WriteLine(calculator.Add(1, 2));
+            System.Console.WriteLine(calculator.Add(1, 2, 3));
+            System.Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            System.Console.WriteLine(calculator.Add(5, 444, 5332));
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(new Point(40, 60));
+                System.Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                System.Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            }
+            catch (System.Exception)
+            {
+
+                System.Console.WriteLine("An unexpected error occured.");
+            }
         }
     }
 }
